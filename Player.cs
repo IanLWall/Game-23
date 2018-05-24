90a8799861eddeb85016fda13195df77fb0b1ab7
IanLWall <38676270+IanLWall@users.noreply.github.com>
2018-05-23 20:07:33 -0700

    Darkness Scripts
    Patrol - Darkness pathing
    Player - Safespace script
    Destroy - Destroy Player script

---
+++ Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public bool isHidden;
+
+
+	// Use this for initialization
+	void Start () {
+        isHidden = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "safespace")
+        {
+            Debug.Log("Hidden");
+            isHidden = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "safespace")
+        {
+            isHidden = false;
+        }
+    }
+}
